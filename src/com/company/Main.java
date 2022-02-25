ca9f71b7d17d06c9c4e09448ec1adc26b10494cd
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-02-25 20:59:01 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        int n=4;
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+	// write your code here
+    }
+}
